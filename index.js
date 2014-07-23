251e2e6933abe0e0130117b7811ede2c4a6e609c
Frode Danielsen <frode@e5r.no>
2014-07-23 15:51:00 +0200

    First working version

---
+++ index.js
@@ -0,0 +1,40 @@
+var
+    Hogan = require('hogan.js'),
+    path = require('path'),
+    through = require('through2');
+
+var extensions = ['.hogan', '.mustache'];
+
+module.exports = function hoganify (file, options) {
+    if (extensions.indexOf(path.extname(file)) === -1) {
+        return through();
+    }
+
+    var buffer = '';
+
+    function write (chunk, enc, next) {
+        buffer += chunk;
+        next();
+    }
+
+    function end () {
+        var
+            template = null,
+            text = JSON.stringify(buffer),
+            compiled = '';
+
+        compiled += "var Hogan = require('hogan.js');\n";
+        if (options.liveCompile) {
+            compiled += "module.exports = Hogan.compile(" + text + ");";
+        }
+        else {
+            template = Hogan.compile(buffer, { asString: true });
+            compiled += "module.exports = new Hogan.Template(" + template + ", " + text + ", Hogan);";
+        }
+
+        this.push(compiled);
+        this.push(null);
+    }
+
+    return through(write, end);
+};
